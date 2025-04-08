5aee5c78241224771246c1d4a74eaee1e22668b6
Maxim11011 <122366257+Maxim11011@users.noreply.github.com>
2025-04-08 14:57:12 +0300

    Update mkepg.sh rate=20k

--- mkepg.sh
+++ mkepg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm *.json
-wget --no-check-certificate --wait=3 -i ch2.txt
+wget --no-check-certificate --limit-rate=20k -i ch2.txt
 if [ -e channels.xml ]; then
 	echo "File for channels exists, skeeping...";
 else 
