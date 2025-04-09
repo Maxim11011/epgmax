5da4e272d3c98b65a960aaffb29ea33ffb873c16
Maxim11011 <122366257+Maxim11011@users.noreply.github.com>
2025-04-09 11:11:23 +0300

    Update mkepg.sh Android 9

--- mkepg.sh
+++ mkepg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm *.json
-wget -d --no-check-certificate --limit-rate=200k --user-agent="Player (Linux; Android 9)" -i ch2.txt
+wget -d --no-check-certificate --limit-rate=200k --user-agent="Android 9" -i ch2.txt
 if [ -e channels.xml ]; then
 	echo "File for channels exists, skeeping...";
 else 
