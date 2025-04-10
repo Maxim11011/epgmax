5af02f0055b9143a7dc4281b15ed095944cfe139
Maxim11011 <122366257+Maxim11011@users.noreply.github.com>
2025-04-10 11:56:51 +0300

    Update mkepg.sh

--- mkepg.sh
+++ mkepg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm *.json
 wget -d --no-check-certificate --header="Accept-encoding: gzip" --limit-rate=50k -i ch2.txt
-for f in *.json; do
-   mv -- "$f" "${f%.json}.gz"
+for f in *json; do
+   mv -- "$f" "${f%}.json.gz"
 done
 # Decompresses all .gz files in the current directory
 for file in *.gz; do
-  gunzip "$file"  # Decompresses each .gz file found
+  gunzip -f "$file"  # Decompresses each .gz file found
 done
 if [ -e channels.xml ]; then
 	echo "File for channels exists, skeeping...";
