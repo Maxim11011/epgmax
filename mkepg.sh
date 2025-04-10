c9b8c3f58b9be8623b2d2a5e744e8089a8dc5d1f
Maxim11011 <122366257+Maxim11011@users.noreply.github.com>
2025-04-10 11:09:06 +0300

    Update mkepg.sh

--- mkepg.sh
+++ mkepg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm *.json
 wget -d --no-check-certificate --header="Accept-encoding: gzip" --limit-rate=50k -i ch2.txt
-gunzip -f -v *.json\
+mv "*.json" "*.gjson.gz"
+gunzip -f -v "*.gjson.gz"
 if [ -e channels.xml ]; then
 	echo "File for channels exists, skeeping...";
 else 
